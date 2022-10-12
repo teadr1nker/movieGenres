62edfc6e2e3790d9fd36a7b0edbf5cef6244a3b7
Arseny <arsmir999@gmail.com>
2022-10-12 16:36:05 +0300

    regex+stacked

--- python/convert.py
+++ python/convert.py
@@ -12,19 +12,19 @@ id = 0
 for index, row in data.iterrows():
   name = row['title']
   try:
-    year = re.search(r'\((\d*)\)', name).group(1)
+    year = re.search(r'\((\d{4})\)', name).group(1)
     year = int(year)
     name = name.replace(f'({year})', '')
   except:
     year = 0
   if row['genres'] != '(no genres listed)':
-    movies.loc[id] = [row['movieId'], name, year, row['genres']]
+    movies.loc[id] = [row['movieId'], name, year, row['genres'].split('|')]
     id+=1
   print(f'{index}/{len(data)}', end='\r')
   
-  #if id > 1000:
-  #  break
-movies.to_csv('../data/sorted.csv')
+  if id > 10240:
+    break
+movies.to_json('../data/sorted.json')
   
   
 
--- python/stacked.py
+++ python/stacked.py
@@ -3,16 +3,41 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import os
+
+minYear=1960
 
 print('Loading data')
 movies = pd.read_json('../data/sorted.json').sort_values('year')
-
+    
 print('Loaded data')
 #find min
 for year in movies['year']:
-  if year > 0:
+  if year > minYear:
     min = year
     break
+    
+years = list(np.arange(min, movies['year'].max()+1))
+#print(years)
+print('Getting unique genres')
+genres = []
+for row in movies['genres']:
+  for genre in row:
+    if genre not in genres:
+      genres.append(genre)
+          
+#print(genres)
+print('Counting genres')
+data = np.zeros((len(years), len(genres)))
+    
+for index, row in movies.iterrows():
+  if row['year'] > minYear:
+    for genre in row['genres']:
+      data[years.index(row['year']), genres.index(genre)] += 1
+      print(f'{index+1}/{len(movies)}', end='\r')      
+      
+np.save('../data/stacked.npy', data)
 
-years = np.arange(min, movies['year'].max())
-print(years)
+plt.stackplot(years, data.T)
+plt.legend(genres)
+plt.show()
